e8da80efa1d77cb86599bc408406a524ecd813f6
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-19 15:43:47 -0400

    Adds username, email, password inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
+import UserForm from './components/Form';
 
 function App() {
   return (
-
+    <UserForm />
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
 
-const Form = () => {
+const UserForm = () => {
+    return (
+        <Form>
+            {/* username: */}
+            <label>
+                Username: 
+                <input 
+                type='text'
+                name='username'
+                />
+            </label>
 
+            {/* email: */}
+            <label>
+                Email:
+                <input 
+                type='text'
+                name='email'
+                />
+            </label>
+            
+            {/* password: */}
+            <label>
+                Password:
+                <input
+                type='password'
+                name='password'
+                />
+            </label>
+
+
+
+
+
+
+
+
+        </Form>
+        
+    )
 }
 
-export default Form;
+export default UserForm;
